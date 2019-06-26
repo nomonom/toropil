d71007e76c98e85bc2709939cbcbf1c420a23fdb
servicemi <52180716+servicemi@users.noreply.github.com>
2019-06-26 10:49:42 +0100

    Update pusk.sh

--- pusk.sh
+++ pusk.sh
@@ -2,16 +2,16 @@
 echo "Start"
 while (true) 
 do
- ./705.sh;
- sleep 75; 
-  ./500.sh;
- sleep 98; 
-   ./800.sh;
- sleep 85; 
-   ./605.sh;
- sleep 78; 
-   ./900.sh;
- sleep 69; 
-  ./400.sh;
+. ./705.sh;
+ sleep 75;
+. ./500.sh;
+ sleep 98;
+. ./800.sh;
+ sleep 85;
+. ./605.sh;
+ sleep 78;
+. ./900.sh;
+ sleep 69;
+. ./400.sh;
  sleep 99; 
 done;
